1ac934e4b09ee2e93c5c1328ed95395c12e1ac0d
Jin mo Kim <skim112790@yahoo.com>
2022-01-27 21:28:45 -0500

    changed window alerts to modals, added cookies per second, added message for player progression

--- game-assets/script.js
+++ game-assets/script.js
@@ -1,23 +1,49 @@
 var cookieClicker = {
-    cookie: document.querySelector("#cc-img"),
-    countContainer: document.querySelector("#cookies-container"),
+    cookie: document.getElementById("cc-img"),
+    countContainer: document.getElementById("cookies-container"),
+    totalContainer: document.getElementById("total-container"),
+    cpsContainer: document.getElementById("cps-container"),
     click: 1,
     count: 0,
-
+    total: 0,
+    cps: 0,
+    progress: [true, true, true],
     // **Add variable to store amount of cookies spent on upgrades.
     // **Add variable to combine cookies owned + cookies spent for total cookies baked
     // **Add variable for cookies per second. add value whenever passive is upgraded
 
     main() {
-        //Set bakery name
-        var bakery = document.querySelector("#bakery-name")
+        var bakery = document.getElementById("bakery-name")
+
+        // get modal
+        var bakeryModal = document.getElementById("bakery-modal")
+        // get button to open modal
+        var bakeryBtn = document.getElementById("bakery-btn")
+        // get close button
+        var closeBtn = document.getElementsByClassName("close")[0];
+        // open modal on click
+        bakeryBtn.onclick = function() {
+            bakeryModal.style.display = "block";
+        }
+        // close display when close button is pressed
+        closeBtn.onclick = function() {
+            bakeryModal.style.display = "none";
+        }
+        // close display when user clicks outside
+        window.onclick = function(event) {
+            if (event.target == bakeryModal) {
+                bakeryModal.style.display = "none";
+            }
+        }
+        // function to change bakery name
         function changeName(name) {
             bakery.textContent = name
         }
-
-        bakery.addEventListener("click", async function(){
-            let name = prompt("Please enter your bakery name", "Tom's Bakery")  
-            await changeName(name)          
+        var newName = document.getElementById("new-name")
+        var submitName = document.getElementById("submit-name")
+        submitName.addEventListener("click", async function() {
+            await changeName(newName.value)
+            bakeryModal.style.display = "none";
         })
 
         // cookie image event listener
@@ -25,18 +51,38 @@ var cookieClicker = {
 
             // click function
             cookieClicker.cookie.addEventListener("click", function(){
+                // add count to cookies owned
                 cookieClicker.count = cookieClicker.count + cookieClicker.click
+                // add count to cookies total
+                cookieClicker.total = cookieClicker.total + cookieClicker.click
+                // update text
                 cookieClicker.countContainer.textContent = cookieClicker.count + " cookies"
+                cookieClicker.totalContainer.textContent = cookieClicker.total + " cookies baked total"
             })
 
         }
         
         // Function containing all upgrades
         function upgrades() {
+            // get modal
+            var upgradeModal = document.getElementById("upgrade-modal")
+            // get close btn
+            closeBtn = document.getElementsByClassName("close")[0];
+            // close display when close btn is pressed
+            closeBtn.onclick = function() {
+                upgradeModal.style.display = "none";
+            }
+            // close display when user clicks outside
+            window.onclick = function(event) {
+                if (event.target == upgradeModal) {
+                    upgradeModal.style.display = "none";
+                }
+            }
+
 
             // Click power upgrade tags
-            var clickUpgrade = document.querySelector("#click-upgrade")
-            var clickUpgradeDesc = document.querySelector("#click-upgrade-desc")
+            var clickUpgrade = document.getElementById("click-upgrade")
+            var clickUpgradeDesc = document.getElementById("click-upgrade-desc")
 
             // Click upgrade cost
             var c1Cost = 10
@@ -49,7 +95,7 @@ var cookieClicker = {
 
                 // Check if user has enough cookies for upgrade
                 if (cookieClicker.count < c1Cost) {
-                    alert("Not enough cookies!")
+                    upgradeModal.style.display = "block";
                 } else {
                     // Update cookie count, update upgrade cost, update click power, update text 
                     cookieClicker.count = cookieClicker.count - c1Cost
@@ -61,8 +107,8 @@ var cookieClicker = {
             })
 
             // passive income upgrade tags
-            var p1Upgrade = document.querySelector("#p1-upgrade")
-            var p1UpgradeDesc = document.querySelector("#p1-upgrade-desc")
+            var p1Upgrade = document.getElementById("p1-upgrade")
+            var p1UpgradeDesc = document.getElementById("p1-upgrade-desc")
 
             // 1st passive income upgrade cost
             var p1Cost = 20
@@ -71,7 +117,9 @@ var cookieClicker = {
             function p1Clicker() {
                 setInterval(function() {
                     cookieClicker.count = cookieClicker.count + 1
+                    cookieClicker.total = cookieClicker.total + 1
                     cookieClicker.countContainer.textContent = cookieClicker.count + " cookies"
+                    cookieClicker.totalContainer.textContent = cookieClicker.total + " cookies baked total"
                 }, 1000);
             }
             // text description for upgrade cost
@@ -82,11 +130,13 @@ var cookieClicker = {
 
                 // check if user has enough cookies for upgrade
                 if (cookieClicker.count < p1Cost) {
-                    alert("Not enough cookies!")
+                    upgradeModal.style.display = "block";
                 } else {
                     // remove cookies for upgrade, update count, update p1 cost, update text
                     cookieClicker.count = cookieClicker.count - p1Cost
                     cookieClicker.countContainer.textContent = cookieClicker.count + " cookies"
+                    cookieClicker.cps = cookieClicker.cps + 1
+                    cookieClicker.cpsContainer.textContent = cookieClicker.cps + " per second"
                     p1Cost = p1Cost + 20
                     p1UpgradeDesc.textContent = "Upgrade Cost: " + p1Cost
                     p1Clicker()
@@ -94,10 +144,25 @@ var cookieClicker = {
             })
         }
         
+
         // call functions
         cookieImg()
         upgrades()
     }
 }
 
+function statTracker() {
+    setInterval(function() {
+        if (cookieClicker.total >= 10 && cookieClicker.progress[0] == true) {
+        alert("Your neighbors and their kids love your cookies!")
+        cookieClicker.progress[0] = false
+        } 
+        if(cookieClicker.total >= 100 && cookieClicker.progress[1] == true) {
+            alert("Your cookies are becoming famous within the neighborhood.")
+            cookieClicker.progress[1] = false
+        }
+    }, 1000)
+}
+statTracker()
 cookieClicker.main()
+
