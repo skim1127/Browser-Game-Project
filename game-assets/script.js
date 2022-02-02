db41089906aadb44df398b5f2a45743a5df610fe
Jin mo Kim <skim112790@yahoo.com>
2022-02-01 20:41:33 -0500

    added sound effects, added achievement titles

--- game-assets/script.js
+++ game-assets/script.js
@@ -7,8 +7,11 @@ var modals = {
     upgradeModal: document.getElementById("upgrade-modal"),
     progressModal: document.getElementById("progress-modal"),
     bakeryBtn: document.getElementById("bakery-btn"),
-    closeBtn: document.getElementsByClassName("close")[0],
+    closeBtn1: document.getElementsByClassName("close")[0],
+    closeBtn2: document.getElementsByClassName("close")[1],
+    closeBtn3: document.getElementsByClassName("close")[2],
     pmContent: document.getElementById("pm-content"),
+    pmTitle: document.getElementById("pm-title"),
     
     // main modal function
     modalMain() {
@@ -17,10 +20,16 @@ var modals = {
             modals.bakeryModal.style.display = "block";
         }
         // close modal on button click
-        modals.closeBtn.onclick = function() {
-            modals.bakeryModal.style.display = "none";
-            modals.progressModal.style.display = "none";
+        modals.closeBtn1.onclick = function() {
             modals.upgradeModal.style.display = "none";
+        }
+        // close progress modal on click
+        modals.closeBtn2.onclick = function() {
+            modals.progressModal.style.display = "none";
+        }
+        // close name change modal on click
+        modals.closeBtn3.onclick = function() {
+            modals.bakeryModal.style.display = "none";
         }
         // close modal on clicking outside the modal
         window.onclick = function(event) {
@@ -30,19 +39,40 @@ var modals = {
             if (event.target == modals.upgradeModal) {
                 modals.upgradeModal.style.display = "none";
             }
-            if (event.target == modals.progressModal) {
-                modals.progressModal.style.display = "none";
-            }
         }
     },
 
     // progress modal display messages
     prog1: "Your neighbors love your cookies!",
+    prog1Title: '"A Good Neighbor"',
     prog2: "Your cookies are becoming famous within the neighborhood.",
+    prog2Title: '"Hottest Commodity In Town"',
     prog3: "People are visiting from neighboring towns to taste your cookies.",
+    prog3Title: '"A Drive Worthwhile"',
     prog4: "Your bakery now has multiple locations across the state.",
+    prog4Title: '"Too Good To Stay Local"',
     prog5: "Your cookies can now be found in any supermarket or convenience store in the country!",
+    prog5Title: '"Nationwide Cookie Fever"',
     prog6: "You are now a celebrity and people are asking for your signature on their cookies.",
+    prog6Title: '"Cookie Celebrity"',
+}
+
+// add sound function
+function sound(src) {
+    this.sound = document.createElement("audio");
+    this.sound.src = src;
+    this.sound.setAttribute("preload", "auto");//load sound when loading website
+    this.sound.setAttribute("controls", "none");//no controls
+    this.sound.style.display = "none";
+    document.body.appendChild(this.sound);
+    // play function
+    this.play = function() {
+        this.sound.play();
+    }
+    // stop function
+    this.stop = function() {
+        this.sound.pause();
+    }
 }
 
 // Cookie clicker object
@@ -75,6 +105,17 @@ var cookieClicker = {
 
     main() {
         var bakery = document.getElementById("bakery-name")
+        // click sounds
+        var clickSound = new sound("./Sounds/pop.wav")
+        var clickSound2 = new sound("./Sounds/pop.wav")
+        var clickSound3 = new sound("./Sounds/pop.wav")
+        var clickSound4 = new sound("./Sounds/pop.wav")
+        var clickSound5 = new sound("./Sounds/pop.wav")
+        var clickSound6 = new sound("./Sounds/pop.wav")
+        var clickSound7 = new sound("./Sounds/pop.wav")
+        var clickSound8 = new sound("./Sounds/pop.wav")
+        var clickSound9 = new sound("./Sounds/pop.wav")
+        var clickSound10 = new sound("./Sounds/pop.wav")
 
         // function to change bakery name
         function changeName(name) {
@@ -92,6 +133,29 @@ var cookieClicker = {
 
             // click function
             cookieClicker.cookie.addEventListener("click", function(){
+                // brute forcing sound overlaps
+                if (clickSound2.sound.currentTime == 0.291655 || clickSound2.sound.currentTime == 0) {
+                    clickSound2.play()
+                } else if(clickSound3.sound.currentTime == 0.291655 || clickSound3.sound.currentTime == 0) {
+                    clickSound3.play()
+                } else if(clickSound4.sound.currentTime == 0.291655 || clickSound4.sound.currentTime == 0) {
+                    clickSound4.play()
+                } else if(clickSound5.sound.currentTime == 0.291655 || clickSound5.sound.currentTime == 0) {
+                    clickSound5.play()
+                } else if(clickSound6.sound.currentTime == 0.291655 || clickSound6.sound.currentTime == 0) {
+                    clickSound6.play()
+                } else if(clickSound7.sound.currentTime == 0.291655 || clickSound7.sound.currentTime == 0) {
+                    clickSound7.play()
+                } else if(clickSound8.sound.currentTime == 0.291655 || clickSound8.sound.currentTime == 0) {
+                    clickSound8.play()
+                } else if(clickSound9.sound.currentTime == 0.291655 || clickSound9.sound.currentTime == 0) {
+                    clickSound9.play()
+                } else if(clickSound10.sound.currentTime == 0.291655 || clickSound10.sound.currentTime == 0) {
+                    clickSound10.play()
+                } else {
+                    clickSound.play()
+                }
+                
                 // add count to cookies owned
                 cookieClicker.count = cookieClicker.count + cookieClicker.click
                 // add count to cookies total
@@ -102,7 +166,8 @@ var cookieClicker = {
             })
 
         }
-        
+        // declare variable for upgrade sound
+        var upgradeSound = new sound("./Sounds/box-check.wav")
         // Function containing all upgrades
         function upgrades() {
 
@@ -111,7 +176,7 @@ var cookieClicker = {
             var clickUpgradeDesc = document.getElementById("click-upgrade-desc")
 
             // Click upgrade cost
-            var c1Cost = 10
+            var c1Cost = 100
 
             // text description for upgrade cost
             clickUpgradeDesc.textContent = "-" + c1Cost
@@ -123,7 +188,14 @@ var cookieClicker = {
                 if (cookieClicker.count < c1Cost) {
                     modals.upgradeModal.style.display = "block";
                 } else {
-                    // Update cookie count, update upgrade cost, update click power, update text 
+                    // Play Sound 
+                    if (upgradeSound.sound.currentTime > 0) {
+                        var upgradeSound2 = new sound("./Sounds/box-check.wav")
+                        upgradeSound2.play()
+                    } else {
+                        upgradeSound.play()
+                    }
+                    // Update cookie count, update upgrade cost, update click power, update text
                     cookieClicker.count = cookieClicker.count - c1Cost
                     cookieClicker.countContainer.textContent = cookieClicker.count + " cookies"
                     c1Cost = c1Cost * 3
@@ -157,11 +229,20 @@ var cookieClicker = {
 
             // upgrade btn event listener
             p1Upgrade.addEventListener("click", function(){
-
+                if (cookieClicker.p1Count == 10) {
+                    console.log("Hello")
+                }
                 // check if user has enough cookies for upgrade
                 if (cookieClicker.count < p1Cost) {
                     modals.upgradeModal.style.display = "block";
                 } else {
+                    // Play Sound 
+                    if (upgradeSound.sound.currentTime > 0) {
+                        var upgradeSound2 = new sound("./Sounds/box-check.wav")
+                        upgradeSound2.play()
+                    } else {
+                        upgradeSound.play()
+                    }
                     // remove cookies for upgrade, update text
                     cookieClicker.count = cookieClicker.count - p1Cost
                     cookieClicker.p1Count = cookieClicker.p1Count + 1
@@ -220,6 +301,13 @@ var cookieClicker = {
                 if (cookieClicker.count < p2Cost) {
                     modals.upgradeModal.style.display = "block";
                 } else {
+                    // Play Sound 
+                    if (upgradeSound.sound.currentTime > 0) {
+                        var upgradeSound2 = new sound("./Sounds/box-check.wav")
+                        upgradeSound2.play()
+                    } else {
+                        upgradeSound.play()
+                    }
                     // remove cookies for upgrade, update text
                     cookieClicker.count = cookieClicker.count - p2Cost
                     cookieClicker.p2Count = cookieClicker.p2Count + 1
@@ -275,6 +363,13 @@ var cookieClicker = {
                 if (cookieClicker.count < p3Cost) {
                     modals.upgradeModal.style.display = "block";
                 } else {
+                    // Play Sound 
+                    if (upgradeSound.sound.currentTime > 0) {
+                        var upgradeSound2 = new sound("./Sounds/box-check.wav")
+                        upgradeSound2.play()
+                    } else {
+                        upgradeSound.play()
+                    }
                     // remove cookies for upgrade, update text
                     cookieClicker.count = cookieClicker.count - p3Cost
                     cookieClicker.p3Count = cookieClicker.p3Count + 1
@@ -309,7 +404,7 @@ var cookieClicker = {
             var p4Text = document.getElementById("row4-text")
             
             // set base upgrade cost
-            var p4Cost = 6500 //change value later
+            var p4Cost = 10 //change value later
             // set text for upgrade cost
             p4UpgradeDesc.textContent = "-" + p4Cost
 
@@ -330,6 +425,13 @@ var cookieClicker = {
                 if (cookieClicker.count < p4Cost) {
                     modals.upgradeModal.style.display = "block";
                 } else {
+                    // Play Sound 
+                    if (upgradeSound.sound.currentTime > 0) {
+                        var upgradeSound2 = new sound("./Sounds/box-check.wav")
+                        upgradeSound2.play()
+                    } else {
+                        upgradeSound.play()
+                    }
                     // remove cookies for upgrade, update text
                     cookieClicker.count = cookieClicker.count - p4Cost
                     cookieClicker.p4Count = cookieClicker.p4Count + 1
@@ -385,6 +487,13 @@ var cookieClicker = {
                 if (cookieClicker.count < p5Cost) {
                     modals.upgradeModal.style.display = "block";
                 } else {
+                    // Play Sound 
+                    if (upgradeSound.sound.currentTime > 0) {
+                        var upgradeSound2 = new sound("./Sounds/box-check.wav")
+                        upgradeSound2.play()
+                    } else {
+                        upgradeSound.play()
+                    }
                     // remove cookies for upgrade, update text
                     cookieClicker.count = cookieClicker.count - p5Cost
                     cookieClicker.p5Count = cookieClicker.p5Count + 1
@@ -419,44 +528,76 @@ var cookieClicker = {
 }
 
 function statTracker() {
+    var progSound = new sound("./Sounds/bling.wav")
+    var winSound = new sound("./Sounds/long-bling.wav")
     setInterval(function() {
 
         // Progress 1
         if (cookieClicker.total >= 100 && cookieClicker.progress[0] == true) {
+            progSound.play()
             // change text content, display achievement message
             modals.pmContent.textContent = modals.prog1;
+            modals.pmTitle.textContent = modals.prog1Title;
             modals.progressModal.style.display = "block";
             cookieClicker.progress[0] = false;
+            setTimeout(() => {
+                modals.progressModal.style.display = "none"
+            }, 8000);
         } 
         // Progress 2
         if(cookieClicker.total >= 1000 && cookieClicker.progress[1] == true) {
+            progSound.play()
             modals.pmContent.textContent = modals.prog2;
+            modals.pmTitle.textContent = modals.prog2Title;
             modals.progressModal.style.display = "block";
             cookieClicker.progress[1] = false;
+            setTimeout(() => {
+                modals.progressModal.style.display = "none"
+            }, 8000);
         }
         // Progress 3
         if(cookieClicker.total >= 10000 && cookieClicker.progress[2] == true){
+            progSound.play()
             modals.pmContent.textContent = modals.prog3;
+            modals.pmTitle.textContent = modals.prog3Title;
             modals.progressModal.style.display = "block";
             cookieClicker.progress[2] = false;
+            setTimeout(() => {
+                modals.progressModal.style.display = "none"
+            }, 8000);
         }
         // Progress 4
         if(cookieClicker.total >= 100000 && cookieClicker.progress[3] == true){
+            progSound.play()
             modals.pmContent.textContent = modals.prog4;
+            modals.pmTitle.textContent = modals.prog4Title;
             modals.progressModal.style.display = "block";
             cookieClicker.progress[3] = false;
+            setTimeout(() => {
+                modals.progressModal.style.display = "none"
+            }, 8000);
         }
         // Progress 5
         if(cookieClicker.total >= 1000000 && cookieClicker.progress[4] == true){
+            progSound.play()
             modals.pmContent.textContent = modals.prog5;
+            modals.pmTitle.textContent = modals.prog5Title;
             modals.progressModal.style.display = "block";
             cookieClicker.progress[4] = false;
+            setTimeout(() => {
+                modals.progressModal.style.display = "none"
+            }, 8000);
         }
         // Progress 6
         if(cookieClicker.total >= 10000000 && cookieClicker.progress[5] == true){
+            winSound.play()
             modals.pmContent.textContent = modals.prog6;
+            modals.pmTitle.textContent = modals.prog6Title;
             modals.progressModal.style.display = "block";
             cookieClicker.progress[5] = false;
+            setTimeout(() => {
+                modals.progressModal.style.display = "none"
+            }, 8000);
         }
     }, 1000)
 }
